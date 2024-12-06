5769980329c1d9d06033bc80de4266ded878bfca
Sommerauer, Christoph <christoph.cs.sommerauer@gmail.com>
2024-12-06 16:19:18 +0100

    Add files for day 6

---
+++ 6/sol_6.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+#include "../utility.h"
+
+int sol_6_1(const std::string &file_path)
+{
+
+
+    return 0;
+}
+
+
+int sol_6_2(const std::string &file_path)
+{
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 // #include "2/sol_2.cpp"
 // #include "3/sol_3.cpp"
 // #include "4/sol_4.cpp"
-#include "5/sol_5.cpp"
-// #include "6/sol_6.cpp"
+// #include "5/sol_5.cpp"
+#include "6/sol_6.cpp"
 // #include "7/sol_7.cpp"
 // day 8 is solved with python
 // #include "9/sol_9.cpp"
@@ -48,12 +48,12 @@ int main(int /*argc*/, char** /*argv*/)
     std::cout << sol_3_2(get_input_file_name(3)) << std::endl;
     std::cout << sol_4_1(get_input_file_name(4)) << std::endl;
     std::cout << sol_4_2(get_input_file_name(4)) << std::endl;
-    */
     std::cout << sol_5_1(get_input_file_name(5)) << std::endl;
     std::cout << sol_5_2(get_input_file_name(5)) << std::endl;
-    /*
+    */
     std::cout << sol_6_1(get_input_file_name(6)) << std::endl;
     std::cout << sol_6_2(get_input_file_name(6)) << std::endl;
+    /*
     std::cout << sol_7_1(get_input_file_name(7)) << std::endl;
     std::cout << sol_7_2(get_input_file_name(7)) << std::endl;
     std::cout << sol_9_1(get_input_file_name(9)) << std::endl;
